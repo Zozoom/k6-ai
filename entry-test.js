779b3aaec0c88ad740b39aa9d25d0eb9f0766870
Zoltan Kiss <kiss.zoltan.pti@gmail.com>
2024-01-24 23:41:06 +0100

    push

--- entry-test.js
+++ entry-test.js
@@ -2,8 +2,8 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export let options = {
-    vus: 4, // Number of virtual users
-    duration: '10s', // Duration of the test
+    vus: 1, // Number of virtual users
+    duration: '2s', // Duration of the test
 };
 
 export default function () {
@@ -11,6 +11,5 @@ export default function () {
 
     // Check if the response status was 200 (OK)
     check(response, { 'status was 200': (r) => r.status == 200 });
-
     sleep(1); // Wait for 1 second between requests
 }
